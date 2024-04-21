6ae9b2e62ac96655163f0b30ce829b79a5f90d74
Tungsten Rust <tungsten.rust@gmail.com>
2024-04-21 10:43:05 -0700

    get all comments by id is ready

--- src/main/java/com/springboot/blog/springbootblogrestapi/repository/CommentRepository.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/repository/CommentRepository.java
@@ -1,9 +1,12 @@
 package com.springboot.blog.springbootblogrestapi.repository;
 
+import com.springboot.blog.springbootblogrestapi.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface CommentRepository extends JpaRepository<CommentRepository, Long> {
-
+List<Comment> findByPostId(long postId);
 
 }
--- src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/CommentServiceImpl.java
@@ -9,6 +9,9 @@ import com.springboot.blog.springbootblogrestapi.repository.PostRepository;
 import com.springboot.blog.springbootblogrestapi.service.CommentService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CommentServiceImpl implements CommentService {
     private CommentRepository commentRepository;
@@ -31,6 +34,15 @@ public class CommentServiceImpl implements CommentService {
         Comment newComment = commentRepository.save(comment);
         return mapToDto(newComment);
     }
+
+    @Override
+    public List<CommentDto> getCommentsByPostId(long postId) {
+        //retrieve comments by postId
+        List<Comment> comments = commentRepository.findByPostId(postId);
+        // convert list of comment entities to list of comment dto's
+        return comments.stream().map(comment -> mapToDto(comment)).collect(Collectors.toList());
+    }
+
     private CommentDto mapToDto(Comment comment) {
         CommentDto commentDto = new CommentDto();
         commentDto.setId(comment.getId());
