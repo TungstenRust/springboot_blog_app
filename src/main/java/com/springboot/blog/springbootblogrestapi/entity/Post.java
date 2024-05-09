4f23f9abe59c6cc1c6e7cc4fcca7296a304f900a
Tungsten Rust <tungsten.rust@gmail.com>
2024-05-09 03:31:50 -0700

    category entity is added

--- src/main/java/com/springboot/blog/springbootblogrestapi/entity/Post.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/entity/Post.java
@@ -30,4 +30,8 @@ public class Post {
     //List allows Duplicates, set doesn't allow duplicates
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Comment> comments = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
