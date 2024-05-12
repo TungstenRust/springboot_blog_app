73aa0be46611ade5e55df1072061712c2bd18d90
Tungsten Rust <tungsten.rust@gmail.com>
2024-05-12 05:23:47 -0700

    Swagger Documentation for Postcontroller is finshed but others left

--- src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
@@ -3,29 +3,55 @@ import com.springboot.blog.springbootblogrestapi.payload.PostDto;
 import com.springboot.blog.springbootblogrestapi.payload.PostResponse;
 import com.springboot.blog.springbootblogrestapi.service.PostService;
 import com.springboot.blog.springbootblogrestapi.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+// SWAGGER Documentationu AuthController, CategoryController ve CommenController ucun leksiyada demir ozun yaz
+// PostController-deki SWAGGER Documentationa baxa-baxa arashdira-arashdira ozun yaz
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
+@Tag(
+        name = "CRUD REST APIs for Post Resource"
+)
 public class PostController {
     private PostService postService;
 
     public PostController(PostService postService) {
         this.postService = postService;
     }
-
+    @Operation(
+            summary = "Create Post REST API",
+            description = "Create Post REST API is used to save data in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
     //create blog post rest api
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
+    @Operation(
+            summary = "Get All Posts REST API",
+            description = "GET All Posts By id REST API is used to fetch all Posts from the Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     // get all posts rest api
     @GetMapping
     public PostResponse getAllPosts(
@@ -36,12 +62,30 @@ public class PostController {
     ){
     return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
-
+    @Operation(
+            summary = "Get Post REST API",
+            description = "GET Post By id REST API is used to get single Post from the Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     //get post by id
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    @Operation(
+            summary = "Update Post REST API",
+            description = "Update Post By id REST API is used to update single Post from the Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     //update post by id rest api
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
@@ -49,6 +93,17 @@ public class PostController {
         PostDto postResponse = postService.updatePost(postDto, id);
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
+    @Operation(
+            summary = "Delete Post REST API",
+            description = "Delete Post By id REST API is used to delete single Post from the Database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
     //delete post by id rest api
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
--- src/main/java/com/springboot/blog/springbootblogrestapi/payload/PostDto.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/payload/PostDto.java
@@ -1,4 +1,5 @@
 package com.springboot.blog.springbootblogrestapi.payload;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -6,9 +7,11 @@ import lombok.Data;
 import java.util.Set;
 @Data
 
-
 public class PostDto {
     private long id;
+    @Schema(
+            description = "Blog Post Title"
+    )
     //title should not be null or empty
     //title should have at least 2 characters
     @NotEmpty
@@ -16,12 +19,20 @@ public class PostDto {
     private String title;
     //Post Description should not be null or empty
     //Post Description should have at least 10 characters
+    @Schema(
+            description = "Blog Post Description"
+    )
     @NotEmpty
     @Size(min = 10, message = "Post Description should have at least 10 characters")
     private String description;
+    @Schema(
+            description = "Blog Post Content"
+    )
     // Post content should not be null or empty
     private String content;
     private Set<CommentDto> comments;
-
+    @Schema(
+            description = "Blog Post Category"
+    )
     private Long categoryId;
 }
